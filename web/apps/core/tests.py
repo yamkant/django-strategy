0093ece7cd62aaf625947605a409fd43e5c7584d
yamkant <dev.yamkim@gmail.com>
2023-06-06 19:26:56 +0900

    feat: #4 [Setting: test] integration 테스트를 위한 테스트 설정 (serializer 테스트)

--- web/apps/core/tests.py
+++ web/apps/core/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from typing import Optional
+from django.core.serializers.base import Serializer
 
 # Create your tests here.
 from django.test import TestCase, override_settings
 from members.models import Member
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from core.serializers import CreateSerializer
 
 from django.db import connection, reset_queries
 
@@ -16,9 +19,9 @@ def wrapFunctionForQueryCount(count):
             reset_queries()
             ret = func(*args, **kwargs)
             queries = connection.queries
-            for query in queries:
-                print(f"QUERY: {query['sql']}, TIME: {query['time']}")
-            assert len(queries) == count, "query_count:%d != %d" % (len(queries), count)
+            # for query in queries:
+            #     print(f"QUERY: {query['sql']}, TIME: {query['time']}")
+            assert len(queries) == count, "QUERY COUNT:%d != %d" % (len(queries), count)
             return ret
         return wrapper
     return decorator
@@ -69,3 +72,31 @@ class E2EBaseTestCase(TestCase):
         if not token:
             return {}
         return {"HTTP_AUTHORIZATION": f'Bearer {token["access"]}'}
+
+class IntegrationBaseTestCase(TestCase):
+    def serializer_test(
+        self,
+        expectedQueryCount: Optional[int] = None,
+        serializer: Optional[Serializer] = None,
+        expectedResult: Optional[bool] = None,
+        **data,
+    ):
+        def createData(serializer, data):
+            targetSerializer = serializer(data=data)
+            if targetSerializer.is_valid():
+                instance = targetSerializer.create(data)
+                return instance
+            else:
+                return None
+        if expectedQueryCount:
+            createData = wrapFunctionForQueryCount(expectedQueryCount)(createData)
+
+        instance = None
+        testFlag = False
+        if serializer.serializer_type == "create":
+            instance = createData(serializer, data)
+            if instance:
+                testFlag = True
+        self.assertEqual(expectedResult, testFlag)
+        return instance
+    
