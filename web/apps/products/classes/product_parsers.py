f23eb90244821bf3ea349b993b790d29b4b6cd8c
yamkant <dev.yamkim@gmail.com>
2023-07-23 01:12:28 +0900

    feat: #9 [App: products] 링크를 통해 상품 정보 파싱하는 클래스 수정 및 테스트 추가

---
+++ web/apps/products/classes/product_parsers.py
@@ -0,0 +1,59 @@
+from typing import Any
+import requests
+from bs4 import BeautifulSoup
+from enum import Enum
+
+class OgDataParserException(Exception):
+    pass
+
+
+class OgDataParser:
+    def __init__(self, html_text) -> None:
+        self._soup = BeautifulSoup(html_text, 'html.parser')
+    
+    # NOTE: validation 영역 추가하기
+    def _get_valid_soup(self):
+        if self._soup.head == None:
+            raise OgDataParserException("head 태그가 없습니다.")
+        return self._soup.head
+
+
+    # FIXME: head가 없는 경우, og_data가 NoneType인 경우, og_data가 'content' 키를 갖지 않는 경우 처리
+    def get_og_data_from_soup(self, property):
+        valid_soup = self._get_valid_soup() 
+        og_data = valid_soup.select_one(f'meta[property="og:{property}"]')
+
+        if og_data.get("content") == None:
+            raise OgDataParserException("og data에 content가 없습니다.")
+        return og_data.get("content")
+
+class ProductInfoParser:
+    def __init__(self, title):
+        self._title = title
+
+    def parse_brand(self):
+        self._title
+
+
+class ParsingHandler:
+    """URL로부터 정보를 파싱하는 클래스입니다."""
+    def __init__(self, url) -> None:
+        self._url = url
+        self._html_text = ""
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def html_text(self):
+        return self._html_text
+    
+    def set_info_from_url(self):
+        headers = {'Accept': '*/*'}
+        response = requests.get(self._url, headers=headers)
+        self._html_text = response.text
+    
+    def get_info_by_property(self, property):
+        og_data_parser = OgDataParser(self.html_text)
+        return og_data_parser.get_og_data_from_soup(property)
--- web/apps/products/tests/unittest.py
+++ web/apps/products/tests/unittest.py
@@ -1,29 +1,53 @@
 import logging
 import unittest
+from unittest import skip
 from unittest.mock import Mock
 from enum import Enum
 
-from products.classes.ParsingHandler import ParsingHandler, OgDataParser
+from products.classes.product_parsers import ParsingHandler, OgDataParser, OgDataParserException
 
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-# logger.addHandler(logging.FileHandler('./test.log'))
+# logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.FileHandler('./test.log'))
 logger.setLevel(logging.INFO)
 
+# NOTE: fixture 사용해서 테스트 전처리값 변경
 class OgDataPraserTest(unittest.TestCase):
     def test_get_og_data_from_soup(self):
         expected_og_title = "[TIME HOMME] 와이드 데님 팬츠"
         expected_og_image = "https://cdn-img.thehandsome.com/studio/goods/TH/2C/FW/TH2C8NPC677N_DN_W01.jpg?rs=684X1032"
         html_text = f'''
+        <head>
             <meta data-n-head="ssr" data-hid="og:title" property="og:title" content="{expected_og_title}">
             <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="&amp;nbsp;">
             <meta data-n-head="ssr" data-hid="og:image" property="og:image" content="{expected_og_image}">
             <meta data-n-head="ssr" data-hid="og:type" property="og:type" content="product">
             <meta data-n-head="ssr" data-hid="og:url" property="og:url" content="">
+        </head>
         '''
         og_data_parser = OgDataParser(html_text)
         self.assertEqual(og_data_parser.get_og_data_from_soup('title'), expected_og_title)
         self.assertEqual(og_data_parser.get_og_data_from_soup('image'), expected_og_image)
+
+    def test_get_og_data_without_content(self):
+        expected_og_title = "[TIME HOMME] 와이드 데님 팬츠"
+        expected_og_image = "https://cdn-img.thehandsome.com/studio/goods/TH/2C/FW/TH2C8NPC677N_DN_W01.jpg?rs=684X1032"
+        html_text = f'''
+        <head>
+            <meta data-n-head="ssr" data-hid="og:title" property="og:title"">
+            <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="&amp;nbsp;">
+            <meta data-n-head="ssr" data-hid="og:image" property="og:image" content="{expected_og_image}">
+            <meta data-n-head="ssr" data-hid="og:type" property="og:type" content="product">
+            <meta data-n-head="ssr" data-hid="og:url" property="og:url" content="">
+        </head>
+        '''
+        og_data_parser = OgDataParser(html_text)
+        self.assertRaisesRegex(
+            OgDataParserException,
+            "og data에 content가 없습니다.",
+            og_data_parser.get_og_data_from_soup,
+            'title',
+        )
 
 
 class ParsingHandlerTest(unittest.TestCase):
@@ -32,7 +56,32 @@ class ParsingHandlerTest(unittest.TestCase):
         parse_handler = ParsingHandler(endPoint)
         expected_og_title = "[TIME HOMME] 와이드 데님 팬츠"
         expected_og_image = "https://cdn-img.thehandsome.com/studio/goods/TH/2C/FW/TH2C8NPC677N_DN_W01.jpg?rs=684X1032"
-        self.assertEqual(parse_handler.get_info(), {
-            "title": expected_og_title,
-            "image": expected_og_image,
-        })
+
+        parse_handler.set_info_from_url()
+        self.assertEqual(parse_handler.get_info_by_property('title'), expected_og_title)
+        self.assertEqual(parse_handler.get_info_by_property('image'), expected_og_image)
+
+    @skip
+    def test_get_info_ssg(self):
+        endPoint = 'https://www.ssg.com/item/itemView.ssg?itemId=1000483217140&siteNo=7008&salestrNo=6005'
+        parse_handler = ParsingHandler(endPoint)
+        parse_handler.set_info_from_url()
+        print(parse_handler.html_text)
+        # logger.info(parse_handler.get_info_by_property('title'))
+        # expected_og_title = "[TIME HOMME] 와이드 데님 팬츠"
+        # expected_og_image = "https://cdn-img.thehandsome.com/studio/goods/TH/2C/FW/TH2C8NPC677N_DN_W01.jpg?rs=684X1032"
+        # self.assertEqual(parse_handler.get_info_from_url(), {
+        #     "title": expected_og_title,
+        #     "image": expected_og_image,
+        # })
+    
+    def test_get_info_lemaire(self):
+        endPoint = 'https://us.lemaire.fr/products/belted-easy-pants-bk999-black-m-23s-1'
+        parse_handler = ParsingHandler(endPoint)
+        parse_handler.set_info_from_url()
+
+        expect_og_title = "BELTED EASY PANTS"
+        expect_og_image = "https://us.lemaire.fr/cdn/shop/products/PA1021_LF1019_BK999_4_600x.webp?v=1673372650"
+
+        self.assertEqual(parse_handler.get_info_by_property('title'), expect_og_title)
+        self.assertEqual(parse_handler.get_info_by_property('image'), expect_og_image)
